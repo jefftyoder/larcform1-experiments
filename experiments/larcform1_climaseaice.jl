78ae9fd1086f5bf2d6c8ab18e73303a5ae939e9f
jefftyoder <jeffreyyoder@unr.edu>
2026-06-30 13:44:33 -0700

    create new sea ice experiment

---
+++ experiments/larcform1_climaseaice.jl
@@ -0,0 +1,6 @@
+# This file builds and executes to Larcform1 experiment (Pithan 2016) using ClimaAtmos.jl for the atmosphere and ClimaSeaIce.jl for the SeaIce component.
+# see https://github.com/NumericalEarth/NumericalEarth.jl/blob/main/examples/coupled_conservation.jl for a prior SCM atmosphere + sea ice example
+
+using ClimaAtmos
+using ClimaSeaIce
+
